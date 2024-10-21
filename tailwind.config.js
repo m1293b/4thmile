06544dbe556859ee1f11ae3ce5aa1a8fe66d3036
m1293b <m1293b@gmail.com>
2024-10-21 18:14:49 +0100

    configured Tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./templates/**/*.{html,js}", "./assets/**/*.{html,js}"],
+  content: ["./**/templates/**/*.{html,js}", "./**/assets/**/*.{html,js}"],
   theme: {
     extend: {},
   },
