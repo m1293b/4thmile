649624c648d9ff2209b072815eba019b85c793d3
m1293b <m1293b@gmail.com>
2024-10-21 09:32:36 +0100

    renamed project

--- babysuite/asgi.py
+++ babysuite/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for homelab project.
+ASGI config for babysuite project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
--- babysuite/urls.py
+++ babysuite/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for homelab project.
+URL configuration for babysuite project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
