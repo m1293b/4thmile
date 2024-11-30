cf5a497fd0edb23502fae717217eafbd2157ce85
m1293b <m1293b@m1293b.com>
2024-11-30 20:34:04 +0000

    fixed the base paths for the static files for Django

--- babysuite/settings.py
+++ babysuite/settings.py
@@ -166,11 +166,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/assets/'
 
-STATICFILES_DIRS = [ os.path.join(BASE_DIR,'static') ]
+STATICFILES_DIRS = [ os.path.join(BASE_DIR,'assets') ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets/*')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
