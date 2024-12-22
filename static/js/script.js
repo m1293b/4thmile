c11fce0ee782c0eaa425c46bf5df4f266dcf5775
m1293b <m1293b@m1293b.com>
2024-12-22 05:11:24 +0000

    created a button to show nav-links on small screens, installed jQuery

--- static/js/script.js
+++ static/js/script.js
@@ -2,3 +2,16 @@ window.addEventListener('scroll', function() {
     var navbar = document.querySelector('.navbar');
     navbar.classList.toggle('navbar-sticky', window.scrollY > 0);
 });
+
+// Hamburger menu script
+
+document.getElementById('hamburger-menu').addEventListener('click', function() {
+    this.classList.toggle('active');
+    $('.fa-bars').is(':visible') ? $('.fa-bars').slideUp(100) : $('.fa-bars').slideDown(100);
+    $('.fa-chevron-down').is(':visible') ? $('.fa-chevron-down').slideUp(100) : $('.fa-chevron-down').slideDown(100); 
+    if (this.classList.contains('active')) {
+        document.querySelector('.nav-links').style.display = 'block';
+    } else {
+        document.querySelector('.nav-links').style.display = 'none';
+    }
+});
